733ddf3ecffbc7ed025ae0886c21818efd7f3c5c
Roi Rav-Hon <roi@logz.io>
2016-10-30 20:16:36 +0200

    Another (and correct) test change

--- src/test/java/io/logz/LogzioSenderTest.java
+++ src/test/java/io/logz/LogzioSenderTest.java
@@ -127,8 +127,8 @@ public class LogzioSenderTest extends BaseTest {
         File tempDirectoryThatWillBeInTheSameFsAsTheBuffer = TestEnvironment.createTempDirectory();
         tempDirectoryThatWillBeInTheSameFsAsTheBuffer.deleteOnExit();
 
-        int fsPercentDrop = ((int) (((double) tempDirectoryThatWillBeInTheSameFsAsTheBuffer.getUsableSpace() /
-                tempDirectoryThatWillBeInTheSameFsAsTheBuffer.getTotalSpace()) * 100)) + 10;
+        int fsPercentDrop = 100 - ((int) (((double) tempDirectoryThatWillBeInTheSameFsAsTheBuffer.getUsableSpace() /
+                tempDirectoryThatWillBeInTheSameFsAsTheBuffer.getTotalSpace()) * 100)) - 1;
 
         String message1 = "First log that will be dropped - " +  random(5);
         String message2 = "And a second drop - " + random(5);
