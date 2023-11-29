70ce73ab2bb799ba7da797d598cb2ebd188856a4
ralongit <ralgotmail@gmail.com>
2023-11-29 12:09:38 +0200

    Modify test to support new exception format
    - Created a more dynamic exception test because it fails due to:
      - The expected version is java.lang.Thread.run(Thread.java:750)
      - The actual version in the failure is java.base/java.lang.Thread.run(Thread.java:829)

--- src/test/java/io/logz/logback/LogzioLogbackAppenderTest.java
+++ src/test/java/io/logz/logback/LogzioLogbackAppenderTest.java
@@ -315,35 +315,36 @@ public class LogzioLogbackAppenderTest extends BaseLogbackAppenderTest {
     public void checkExactStackTrace() throws Exception {
         final CountDownLatch countDownLatch = new CountDownLatch(1);
         final MyRunner.ExceptionGenerator exceptionGenerator = new MyRunner.ExceptionGenerator();
-
+    
         // We need to generate an exception with constant stack trace
         new Thread(new MyRunner(countDownLatch, exceptionGenerator)).start();
         countDownLatch.await();
-
+    
         String token = "exceptionToken";
         String type = "stacktraceType" + random(8);
         String loggerName = "traceLogger" + random(8);
         int drainTimeout = 1;
-
+    
         String message1 = "Any line change here can cause the test to break";
-
-        String expectedException = "java.lang.RuntimeException: Got NPE!\n" +
+    
+        String expectedExceptionStart = "java.lang.RuntimeException: Got NPE!\n" +
                 "\tat io.logz.logback.MyRunner$ExceptionGenerator.generateNPE(MyRunner.java:33)\n" +
-                "\tat io.logz.logback.MyRunner.run(MyRunner.java:18)\n" +
-                "\tat java.lang.Thread.run(Thread.java:750)\n" +
-                "Caused by: java.lang.NullPointerException: null\n" +
+                "\tat io.logz.logback.MyRunner.run(MyRunner.java:18)\n";
+        String expectedExceptionEnd = "Caused by: java.lang.NullPointerException: null\n" +
                 "\tat io.logz.logback.MyRunner$ExceptionGenerator.generateNPE(MyRunner.java:31)\n" +
                 "\t... 2 common frames omitted\n";
-
+    
         Logger testLogger = createLogger(logzioLogbackAppender, token, type, loggerName, drainTimeout, false, false, null, false);
-
+    
         testLogger.info(message1, exceptionGenerator.getE());
         sleepSeconds(drainTimeout * 2);
-
+    
         mockListener.assertNumberOfReceivedMsgs(1);
         MockLogzioBulkListener.LogRequest logRequest = mockListener.assertLogReceivedByMessage(message1);
         mockListener.assertLogReceivedIs(logRequest, token, type, loggerName, Level.INFO.levelStr);
-        assertThat(logRequest.getStringFieldOrNull("exception")).isEqualTo(expectedException);
+        String actualException = logRequest.getStringFieldOrNull("exception");
+        assertThat(actualException).startsWith(expectedExceptionStart);
+        assertThat(actualException).endsWith(expectedExceptionEnd);
     }
 
     @Test
